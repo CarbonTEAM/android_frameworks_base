e4e7e141f1906820101a198f2414fabbfa17aff0
Cristiano Matos <cristianobmatos@gmail.com>
2015-11-19 22:09:45 +0100

    Add the ability to hide superuser status bar icon [1/2]
    - Add a simple option to hide the su icon placed in status bar when a
    session is active
    
    Forward ported from crDroid sources:
    https://github.com/crdroidandroid/android_frameworks_base/commit/d8a9b6dedc2bc009a152d942a71b2dfb62a24b53
    
    Change-Id: I68c90ac0fb4a83f613ea0d57d0319dddff3f3990

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4645,6 +4645,11 @@ public final class Settings {
         public static final String GESTURE_ANYWHERE_FLOATING = "gesture_anywhere_floating";
 
         /**
+         * @hide
+         */
+        public static final String SHOW_SU_INDICATOR = "show_su_indicator";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
